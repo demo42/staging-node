012bc4bc0e4b9ff9fdde66542e1cf7eef819cfd0
Steve Lasker <StevenLasker@Hotmail.com>
2019-11-04 23:48:15 -0800

    updates

--- import.sh
+++ import.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
 set -e
-# SP, PASSWORD , CLUSTER_NAME, CLUSTER_RESOURCE_GROUP
-az configure --defaults acr=$ACR_NAME
 
+# SP, PASSWORD , CLUSTER_NAME, CLUSTER_RESOURCE_GROUP
+#echo SP: ${SP}
+#echo PASSWORD: ${PASSWORD}
+#echo TENANT: ${TENANT}
 az login \
     --service-principal \
     --username $SP \
     --password $PASSWORD \
-    --tenant $TENANT  #> /dev/null
+    --tenant $TENANT  > /dev/null
 
-echo -- az acr import --
+az configure --defaults acr=$ACR_NAME
+
+echo -- az acr import --source mcr.microsoft.com/azure-cli:2.0.75 \
+    -t base-artifacts/azure-cli:2.0.75 \
+    --force --
 az acr import \
     --source mcr.microsoft.com/azure-cli:2.0.75 \
-    -t base-artifacts/azure-cli:2.0.75
+    -t base-artifacts/azure-cli:2.0.75 \
+    --force
