a8cf49c46482e509510862351c49e35e4cac1970
steve lasker <stevenlasker@hotmail.com>
2019-11-06 21:54:05 -0800

    fix red

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo BACKGROUND_COLOR: ${BACKGROUND_COLOR}
 echo NODE_VERSION: ${NODE_VERSION}
-if [ ""${BACKGROUND_COLOR} = 'pink' ]; then
+if [ ""${BACKGROUND_COLOR} = 'red' ]; then
     echo "Invalid Color:" ${BACKGROUND_COLOR}
     EXIT_CODE=1
 fi
