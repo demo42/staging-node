6ee2636ce7d90b2126bdb1f30585e91b4084fcb6
steve lasker <stevenlasker@hotmail.com>
2019-11-06 20:06:49 -0800

    test

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,3 @@
 FROM demo42upstream.azurecr.io/library/node:9-alpine
+WORKDIR /test
+COPY ./test.sh .
---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+TEST=foo
+echo BACKGROUND_COLOR: ${BACKGROUND_COLOR}
+echo NODE_VERSION: ${NODE_VERSION}
+echo TEST: ${TEST}
