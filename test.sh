aead35cab4cf4dfe17c3764de5504d47e8b2c01b
steve lasker <stevenlasker@hotmail.com>
2019-11-06 22:28:58 -0800

    add -e

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-echo "\e[32mValiddating Image\e[0m"
+echo -e "\e[32mValiddating Image\e[0m"
 
 echo NODE_VERSION: ${NODE_VERSION}
 echo BACKGROUND_COLOR: ${BACKGROUND_COLOR}
 
 if [ ""$(echo $BACKGROUND_COLOR | tr '[:lower:]' '[:upper:]') = 'RED' ]; then
-    echo "\e[31mERROR: Invalid Color:\e[0m" ${BACKGROUND_COLOR}
+    echo -e "\e[31mERROR: Invalid Color:\e[0m" ${BACKGROUND_COLOR}
     EXIT_CODE=1
 fi
 
