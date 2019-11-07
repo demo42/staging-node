3850ab657e18a245df22996693481936bff2d2bf
steve lasker <stevenlasker@hotmail.com>
2019-11-06 20:46:41 -0800

    test conditional

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
-TEST=foo
+#!/bin/bash
 echo BACKGROUND_COLOR: ${BACKGROUND_COLOR}
 echo NODE_VERSION: ${NODE_VERSION}
-echo TEST: ${TEST}
+if [ ""${BACKGROUND_COLOR^^} = 'RED' ]; then
+    echo "Invalid Color:" ${BACKGROUND_COLOR}
+    EXIT_CODE=1
+fi
+
+exit ${EXIT_CODE}
