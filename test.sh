ad3388f8419b310a98183d8e919a61ac5a5b7d6a
steve lasker <stevenlasker@hotmail.com>
2019-11-06 20:51:02 -0800

    Test

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ echo BACKGROUND_COLOR: ${BACKGROUND_COLOR}
 echo NODE_VERSION: ${NODE_VERSION}
 if [ ""${BACKGROUND_COLOR^^} = 'RED' ]; then
     echo "Invalid Color:" ${BACKGROUND_COLOR}
-    EXIT_CODE=1
+    EXIT_CODE=0
 fi
 
 exit ${EXIT_CODE}
